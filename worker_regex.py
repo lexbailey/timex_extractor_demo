3a13a51f5c33007d265daf4610df49a7a34b28cc
Daniel Bailey <daniel.bailey@arm.com>
2019-10-30 18:00:00 +0000

    Should probably work

--- worker_regex.py
+++ worker_regex.py
@@ -4,17 +4,10 @@ from browser import bind, self
 
 from regex_parser import find_timexes as re_find_timexes
 from regex_data import timex_res, anti_timex_res
-from bayes_extractor import find_timexes as bayes_find_timexes
-from bayes_data import get_tables as bayes_tables
 
 def run_regex(toks):
     s = [{'token':t} for t in toks]
     result = re_find_timexes(s, timex_res, anti_timex_res, quiet=True)
-    return result
-
-def run_bayes(toks):
-    s = [{'token':t} for t in toks]
-    result = bayes_find_timexes(s, 0.13, bayes_tables())
     return result
 
 @bind(self, "message")
@@ -22,9 +15,7 @@ def message(evt):
     c, *data = evt.data
     if c == 'run':
         toks = data[0]
-        result = run_bayes(toks)
-        self.send(['bayes', toks, result])
         result = run_regex(toks)
         self.send(['regex', toks, result])
 
-self.send(["loaded"])
+self.send(["loaded_regex"])
